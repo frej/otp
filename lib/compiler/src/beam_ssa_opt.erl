bf37b45efd3e7b893fe17066ca0d8efaf81a60a1
Frej Drejhammar <frej@sics.se>
2020-02-19 13:55:57 +0100

    beam_ssa_opt: Do aggressive def sinking
    Sometimes values are defined long before their use, this pass tries to
    aggressively sink them to the basic block which dominates all
    uses. This transform is particularly useful for cleaning up after
    ssa_opt_bool_bifs_to_fc. Compared to ssa_opt_sink which only looks at
    get_tuple_element this pass considers any define which can be
    considered safe to move.

--- lib/compiler/src/beam_ssa_opt.erl
+++ lib/compiler/src/beam_ssa_opt.erl
@@ -278,6 +278,7 @@ repeated_passes(Opts) ->
           ?PASS(ssa_opt_cse),
           ?PASS(ssa_opt_tail_phis),
           ?PASS(ssa_opt_sink),
+          ?PASS(ssa_opt_inter_block_sink),
           ?PASS(ssa_opt_tuple_size),
           ?PASS(ssa_opt_record),
           ?PASS(ssa_opt_type_continue)],        %Must run after ssa_opt_dead to
@@ -2287,6 +2288,92 @@ is_merge_allowed_1(L, #b_blk{last=#b_br{}}=Blk, #b_blk{is=Is}) ->
     end;
 is_merge_allowed_1(_, #b_blk{last=#b_switch{}}, #b_blk{}) ->
     false.
+
+%%%
+%%% Sometimes values are defined long before their use, this pass
+%%% tries to aggressively sink them to the basic block which dominates
+%%% all uses. This transform is particularly useful for cleaning up
+%%% after ssa_opt_bool_bifs_to_fc. Compared to ssa_opt_sink which only
+%%% looks at get_tuple_element this pass considers any define which
+%%% can be considered safe to move.
+%%%
+ssa_opt_inter_block_sink({#opt_st{ssa=Linear}=St, FuncDb}) ->
+    Blocks0 = maps:from_list(Linear),
+
+    %% Create a map with all variables mapped to the block the
+    %% variable is defined in.
+    Defs1 = safe_block_definitions(Blocks0),
+
+    %% Now find all the blocks that use the variables we just have
+    %% found.
+    Used1 = used_blocks(Linear, Defs1, []),
+
+    %% It is not safe to move instructions to blocks that begin with
+    %% certain instructions. It is also unsafe to move the
+    %% instructions into any part of a receive. To avoid such unsafe
+    %% moves, pretend that the unsuitable blocks are not dominators.
+    Unsuitable = unsuitable(Linear, Blocks0),
+
+    %% Filter out defs which have a use in the block they are defined
+    %% in or no uses at all, as we can't gain anything from sinking
+    %% them. Also don't try to sink defs in blocks which are unsuitable
+    UsedMap = maps:from_list(Used1),
+    Defs = maps:filter(
+             fun(Var, L) ->
+                     case maps:get(Var, UsedMap, []) of
+                         [] -> false;
+                         Us -> case member(L, Us) of
+                                   true -> false;
+                                   false ->
+                                       not gb_sets:is_element(L, Unsuitable)
+                               end
+                     end
+             end, Defs1),
+
+    %% Filter out uses of non-sinkable defs
+    Used = lists:filter(fun({Var,_}) -> maps:is_key(Var, Defs) end, Used1),
+
+    %% Calculate dominators.
+    {Dom,Numbering} = beam_ssa:dominators(Blocks0),
+
+    %% Calculate new positions for the defining instructions. The new
+    %% position is a block that dominates all uses of the variable.
+    DefLoc = new_def_locations(Used, Defs, Dom, Numbering, Unsuitable),
+
+    %% Now move all suitable defining instructions to their new
+    %% blocks.
+    Blocks = foldl(fun({V,To}, A) ->
+                           From = map_get(V, Defs),
+                           move_defs(V, From, To, A)
+                   end, Blocks0, DefLoc),
+    {St#opt_st{ssa=beam_ssa:linearize(Blocks)}, FuncDb}.
+
+%%%
+%%% Build a map mapping variables to the block they are defined
+%%% in. Only instructions considered safe to move are included in the
+%%% map. Unsafe instructions are instructions with side effects, and
+%%% instructions, which if moved, will prevent common peep-hole
+%%% optimizations from triggering.
+%%%
+safe_block_definitions(Blocks) ->
+    F = fun(L, #b_blk{is=Is}, Acc) -> safe_block_is(Is, L, Acc) end,
+    beam_ssa:fold_rpo(F, #{}, Blocks).
+
+safe_block_is([], _, Acc) ->
+    Acc;
+safe_block_is([#b_set{op=bs_match}|Is], L, Acc) ->
+    safe_block_is(Is, L, Acc);
+safe_block_is([#b_set{op=bs_extract}|Is], L, Acc) ->
+    safe_block_is(Is, L, Acc);
+safe_block_is([#b_set{op=get_hd}|Is], L, Acc) ->
+    safe_block_is(Is, L, Acc);
+safe_block_is([#b_set{op=get_tl}|Is], L, Acc) ->
+    safe_block_is(Is, L, Acc);
+safe_block_is([#b_set{dst=Var}=I|Is], L, Acc) ->
+    case beam_ssa:no_side_effect(I) of
+        true -> safe_block_is(Is, L, Acc#{Var => L});
+        false -> safe_block_is(Is, L, Acc)
+    end.
 
 %%%
 %%% When a tuple is matched, the pattern matching compiler generates a
